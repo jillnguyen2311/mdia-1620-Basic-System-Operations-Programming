9778ba70a416507f2aed80d8cb79cb9fe0ba58e5
jillnguyen2311 <nguyennuduyhoai75@gmail.com>
2022-10-31 11:15:51 -0700

    message

---
+++ week9/For-Loop/for-loop1.js
@@ -0,0 +1,4 @@
+for(var i = 0; i <= 2; i++) {
+    console.log(i);
+    }
+    console.log("finished");
---
+++ week9/For-Loop/for-of-loop.js
@@ -0,0 +1,7 @@
+var weathers = ["sunny","rainy","cloudy"];
+var words="";
+
+for (var days of weathers){
+    words +=days+"\n";
+}
+console.log(words);
